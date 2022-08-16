b1a152ee1923eb560a305b31082c26725d36b798
merleandrade <andrade.merle@gmail.com>
2022-08-16 21:44:35 +0200

    Updated App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,8 @@
-import React, {useState} from 'react';
-import './App.css';
-import axios from "axios";
 
  export default function App() {
 
-  const [message, setMessage] =useState();
-
-  axios.get("/hello")
-      .then((response) => response.data)
-      .then(setMessage)
 
   return (
-    <h1>Hello</h1>
+    <h1>Hallo DU!</h1>
   );
 }
