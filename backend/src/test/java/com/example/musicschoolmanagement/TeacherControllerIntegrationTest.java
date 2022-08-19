ce032634b8aab650c60a1a11f33f74b253e03dd8
merleandrade <andrade.merle@gmail.com>
2022-08-19 11:47:38 +0200

    Fixed IntegrationTest by updating application.properties

--- backend/src/test/java/com/example/musicschoolmanagement/TeacherControllerIntegrationTest.java
+++ backend/src/test/java/com/example/musicschoolmanagement/TeacherControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.example.musicschoolmanagement;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,13 +10,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
- class TeacherControllerIntegrationTest {
-
+class TeacherControllerIntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
@@ -23,7 +22,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     void getAllTeachers() throws Exception {
         mockMvc.perform(get("/api/teachers"))
                 .andExpect(status().isOk())
-        .andExpect(jsonPath("$", hasSize(0)));
+                .andExpect(jsonPath("$", hasSize(0)));
+
     }
 
 }
