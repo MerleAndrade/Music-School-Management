d126668b491edc3a2274e5a67b6950ce8bef2beb
merleandrade <andrade.merle@gmail.com>
2022-08-19 20:47:19 +0200

    Add Integration-Test for add-methode

--- backend/src/test/java/com/example/musicschoolmanagement/TeacherControllerIntegrationTest.java
+++ backend/src/test/java/com/example/musicschoolmanagement/TeacherControllerIntegrationTest.java
@@ -1,10 +1,13 @@
 package com.example.musicschoolmanagement;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -14,11 +17,30 @@ class TeacherControllerIntegrationTest {
     MockMvc mockMvc;
 
     @Test
+    @DisplayName("getAllTeachers")
     void getAllTeachers() throws Exception {
         mockMvc.perform(get("/api/teachers"))
                 .andExpect(status().isOk())
                 .andExpect(content().json( """
                 []
+                """));
+    }
+
+    @Test
+    @DisplayName("AddOneTeacher")
+    void addOneTeacher() throws Exception {
+        mockMvc.perform(post("/api/teachers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"firstName": "Felipe",
+                        "lastName": "Andrade",
+                        "instrument": "Kontrabass"}
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(content().json("""
+                {"firstName": "Felipe",
+                        "lastName": "Andrade",
+                        "instrument": "Kontrabass"}
                 """));
     }
 }
