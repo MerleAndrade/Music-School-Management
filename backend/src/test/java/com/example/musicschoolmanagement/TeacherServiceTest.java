74fd30ed87d93e1cf82fa0abf5f9966d7351273c
merleandrade <andrade.merle@gmail.com>
2022-08-19 12:12:29 +0200

    2. Fixed TeacherServiceTest

--- backend/src/test/java/com/example/musicschoolmanagement/TeacherServiceTest.java
+++ backend/src/test/java/com/example/musicschoolmanagement/TeacherServiceTest.java
@@ -3,12 +3,12 @@ package com.example.musicschoolmanagement;
 import com.example.musicschoolmanagement.model.Teacher;
 import com.example.musicschoolmanagement.model.TeacherRepo;
 import com.example.musicschoolmanagement.model.TeacherService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -18,7 +18,7 @@ class TeacherServiceTest {
     private final TeacherRepo testTeacherRepo = mock(TeacherRepo.class);
     private final TeacherService teacherService = new TeacherService(testTeacherRepo);
 
-    private final List<Teacher> list = List.of(
+    private final List<Teacher> testList = List.of(
             new Teacher("123", "Carlotta", "Meier", "Geige"),
             new Teacher("456", "Linus", "Müller", "Schlagzeug"),
             new Teacher("789", "Josephine", "Huber", "Gesang")
@@ -28,13 +28,13 @@ class TeacherServiceTest {
     @DisplayName("ListOfAllTeacher")
             void getAllTeacher() {
         //given
-        when(testTeacherRepo.findAll()).thenReturn(list);
+        when(testTeacherRepo.findAll()).thenReturn(testList);
 
         //when
         List<Teacher> actual = teacherService.getAllTeachers();
 
         //then
-        Assertions.assertArrayEquals(list.toArray(),actual.toArray());
+        assertThat(actual).hasSameElementsAs(testList);
     }
 
 }
