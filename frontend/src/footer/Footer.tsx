1195cb65a63c57bd5c5e15a02834db4cd6f8432e
merleandrade <andrade.merle@gmail.com>
2022-08-23 15:47:17 +0200

    Updated footer style

--- frontend/src/footer/Footer.tsx
+++ frontend/src/footer/Footer.tsx
@@ -3,12 +3,12 @@ import "./footer.css"
 export default function Footer() {
 
     return (
-        <footer className="footer-content">
-            <img src={"../pictures/muko.png"} className="footer-logo" alt="Muko-Logo" />
-            <p>MUSIK KOLLEKTIV</p>
+        <footer className="footer">
+            <p className="footer-text">
+            <h5>MUSIK KOLLEKTIV</h5>
             <p>Pfeuferstraße 44 | 81371 München | +49 89 123 456 789</p>
             <p>mail@mukomuenchen.de | www.mukomuenchen.de</p>
-
+            </p>
     </footer>
     )
 }
