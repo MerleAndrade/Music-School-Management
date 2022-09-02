20c3771ac15f34eb0fad5fa46ff5879988915315
merleandrade <andrade.merle@gmail.com>
2022-09-02 15:26:43 +0200

    Update Teacher

--- backend/src/main/java/com/example/musicschoolmanagement/teacher/Teacher.java
+++ backend/src/main/java/com/example/musicschoolmanagement/teacher/Teacher.java
@@ -1,9 +1,6 @@
 package com.example.musicschoolmanagement.teacher;
 
 import org.springframework.data.annotation.Id;
-
-
-
 public record Teacher(@Id String id,
         String firstName,
         String lastName,
