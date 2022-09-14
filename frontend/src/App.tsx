d1244e15411c8c300c4638e41b7797eee5522505
merleandrade <andrade.merle@gmail.com>
2022-09-14 21:21:29 +0200

    Clean code here

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-
 import AllRoutes from "./components/AllRoutes";
 import Footer from "./components/Footer";
 import {HashRouter} from "react-router-dom";
@@ -9,13 +8,14 @@ import {ToastContainer} from "react-toastify";
 function App() {
     return (
         <>
-        <HashRouter>
-            <Header/>
-            <AllRoutes/>
-            <Footer/>
-        </HashRouter>
-        <ToastContainer className="toast" position="top-center" style={{width: "150px"}} />
+            <HashRouter>
+                <Header/>
+                <AllRoutes/>
+                <Footer/>
+            </HashRouter>
+            <ToastContainer className="toast" position="top-center" style={{width: "150px"}}/>
         </>
     );
 }
+
 export default App;
